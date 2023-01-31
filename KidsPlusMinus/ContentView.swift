22aa0900ca666fece35a766442f0adf68d9efaa6
Stanislav <te100ff@inbox.ru>
2023-01-31 15:57:14 +0400

    Git test

--- KidsPlusMinus/ContentView.swift
+++ KidsPlusMinus/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Bye, bye world!")
             Text("Are we ready?!")
+            
         }
         .padding()
     }
